a426d4395b817b758a90ed32e8b5de34bbc96cd8
Your Name <you@example.com>
2024-10-30 16:26:41 -0400

    feat: userSignUp

--- src/data/user.ts
+++ src/data/user.ts
@@ -1,6 +1,33 @@
+import { faker } from '@faker-js/faker';
+
+let password = faker.internet.password();
+
 export const user = {
     "name": "Kate",
-    "email": "testkate12@gmail.com",
-    "password": "pass12345",
-    "passwordConfirm":"pass12345"
+    "email": faker.internet.email(),
+    "password": password,
+    "passwordConfirm": password
+}
+
+export function getUser() {
+  
+    let password = faker.internet.password();
+    return {
+        "name": "Kate",
+        "email": faker.internet.email(),
+        "password": password,
+        "passwordConfirm": password
+    }
+
+}
+export function createRandomUser() {
+    return {
+        userId: faker.string.uuid(),
+        username: faker.internet.username(), // before version 9.1.0, use userName()
+        email: faker.internet.email(),
+        avatar: faker.image.avatar(),
+        password: faker.internet.password(),
+        birthdate: faker.date.birthdate(),
+        registeredAt: faker.date.past(),
+    };
 }
--- src/specs/user/signUp.spec.ts
+++ src/specs/user/signUp.spec.ts
@@ -1,26 +1,64 @@
 import * as supertest from 'supertest'
-import { user } from '../../data/user'
+import { getUser, user } from '../../data/user'
 const request = supertest('localhost:8001/api/v1')
-describe('USER SIGNUP',() => {
-    it.skip('Create a new user',async () => {
-        const res = await request.post('/users/signup')
-        .send({
-            "name": "Kate",
-            "email": "testkate11@gmail.com",
-            "password": "pass12345",
-            "passwordConfirm":"pass12345"
-        }).expect(201)
-        expect(res.body.data.user.name).toBe('Kate')
-        expect(res.body.data.user.email).toBe('testkate11@gmail.com')
-        //expect(res.body.data.status).toBe('success')
-        console.log(res.body,'res');
+describe('USER SIGNUP', () => {
+    describe('POSITIVE TESTING', () => {
+        it.skip('Create a new user', async () => {
+            const res = await request.post('/users/signup')
+                .send({
+                    "name": "Kate",
+                    "email": "testkate11@gmail.com",
+                    "password": "pass12345",
+                    "passwordConfirm": "pass12345"
+                }).expect(201)
+            expect(res.body.data.user.name).toBe('Kate')
+            expect(res.body.data.user.email).toBe('testkate11@gmail.com')
+            //expect(res.body.data.status).toBe('success')
+            console.log(res.body, 'res');
+        })
+        it('Create a new user', async () => {
+            const res = await request.post('/users/signup')
+                .send(user).expect(201)
+            expect(res.body.data.user.name).toBe('Kate')
+            expect(res.body.data.user.email).toBe('testkate12@gmail.com')
+            //expect(res.body.data.status).toBe('success')
+            console.log(res.body, 'res');
+        })
+
+        it('Create a new user using faker', async () => {
+            const res = await request.post('/users/signup')
+                .send(user).expect(201)
+            expect(res.body.data.user.name).toBe(user.name)
+            expect(res.body.data.user.email).toBe(user.email.toLowerCase())
+            console.log(res.body, 'res');
+        })
+        it('Create a new user using faker', function (done) {
+            let userImport = getUser()
+            const res = request.post('/users/signup')
+                .send(userImport)
+                .expect(201)
+                .end(function (err, res) {
+                    if (err) return done(err);
+                    console.log(res.body, 'res');
+                    expect(res.body.data.user.name).toBe(userImport.name)
+                    expect(res.body.data.user.email).toBe(userImport.email.toLowerCase())
+
+                    return done()
+                })
+        })
     })
-    it('Create a new user',async () => {
-        const res = await request.post('/users/signup')
-        .send(user).expect(201)
-        expect(res.body.data.user.name).toBe('Kate')
-        expect(res.body.data.user.email).toBe('testkate12@gmail.com')
-        //expect(res.body.data.status).toBe('success')
-        console.log(res.body,'res');
+    describe('NEGATIVE TESTING', () => {
+        it.only('should not create a new user with an existing email', async () => {
+            const res = await request.post('/users/signup').send({
+                "name": "Kate",
+                "email": "testkate11@gmail.com",
+                "password": "pass12345",
+                "passwordConfirm": "pass12345"
+            }).expect(500);
+            expect(res.body.status).toBe('error');
+            expect(res.body.message).toBe("E11000 duplicate key error collection: test.users index: email_1 dup key: { email: \"testkate11@gmail.com\" }")
+
+        })
     })
-})
+    })
+

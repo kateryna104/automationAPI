d72392337da212b4ae3061195ca1296c62f279df
Your Name <you@example.com>
2024-10-30 11:37:58 -0400

    first commit

--- src/data/user.ts
+++ src/data/user.ts
@@ -1,6 +1,6 @@
 export const user = {
-    "name": "Mike",
-    "email": "mike198@gmail.com",
+    "name": "Kate",
+    "email": "testkate12@gmail.com",
     "password": "pass12345",
-    "passwordConfirm": "pass12345"
+    "passwordConfirm":"pass12345"
 }
--- src/specs/user/signUp.spec.ts
+++ src/specs/user/signUp.spec.ts
@@ -1,28 +1,26 @@
 import * as supertest from 'supertest'
 import { user } from '../../data/user'
 const request = supertest('localhost:8001/api/v1')
-
-describe('USER SIGNUP', () => {
-    it.skip('Create a new user', async() => {
-        const res = await request.post("users/signup")
-            .send({
-                "name": "Mike",
-                "email": "mike198@gmail.com",
-                "password": "pass12345",
-                "passwordConfirm": "pass12345"
-            }).expect(201)
-            expect(res.body.data.user.name).toBe('Mike')
-            expect(res.body.data.user.email).toBe('mike198@gmail.com')
-            expect(res.body.status).toBe('success')
-         console.log(res.body,'res');
+describe('USER SIGNUP',() => {
+    it.skip('Create a new user',async () => {
+        const res = await request.post('/users/signup')
+        .send({
+            "name": "Kate",
+            "email": "testkate11@gmail.com",
+            "password": "pass12345",
+            "passwordConfirm":"pass12345"
+        }).expect(201)
+        expect(res.body.data.user.name).toBe('Kate')
+        expect(res.body.data.user.email).toBe('testkate11@gmail.com')
+        //expect(res.body.data.status).toBe('success')
+        console.log(res.body,'res');
     })
-
-    it('Create a new user', async() => {
-        const res = await request.post("users/signup")
-            .send(user).expect(201)
-            expect(res.body.data.user.name).toBe('Mike')
-            expect(res.body.data.user.email).toBe('mike198@gmail.com')
-            expect(res.body.status).toBe('success')
-         console.log(res.body,'res');
+    it('Create a new user',async () => {
+        const res = await request.post('/users/signup')
+        .send(user).expect(201)
+        expect(res.body.data.user.name).toBe('Kate')
+        expect(res.body.data.user.email).toBe('testkate12@gmail.com')
+        //expect(res.body.data.status).toBe('success')
+        console.log(res.body,'res');
     })
 })
